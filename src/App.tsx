0d3a8da462fbf9d4a9a431161e54dc132fd72332
Recyclingbottle <hoholeo382@gmail.com>
2024-08-11 21:08:59 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const App: React.FC = () => {
   return (
     <div>
-      <h1>숨쉬는 다람쥐</h1>
-      <p>hello world</p>
+      <h1>숨쉬는 다람쥐 </h1>
+      <p>CICD TEST</p>
     </div>
   );
 };
