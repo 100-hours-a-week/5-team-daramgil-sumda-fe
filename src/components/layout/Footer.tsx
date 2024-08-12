9c1f595280cbef1effa295d843cc9a56bd19ed41
Harfe0626 <strik241@gmail.com>
2024-08-12 21:15:19 +0900

    feat: 초기 레이아웃 배치 및 CSS 디자인 적용
    - 레이아웃 기본 구조 배치
    - 헤더 디자인 배치
    - 푸터 디자인 배치
    - 네비게이션 바 디자인 배치

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,17 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+import "./styles/Footer.css";
 
 const Footer: React.FC = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <footer className="footer">
+      <img src={logo} alt="Logo" className="footer-logo" />
+      <p className="footer-text">
+        상호명 : 숨쉬는 다람쥐 | 대표자 박원준<br />
+        고객센터 : 010-1234-1234, contact@daramgil.kr<br />
+        주소 : 서울특별시 어딘가 임대로 싼 곳에 있는 다람길
+      </p>
+    </footer>
   );
 };
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import { Outlet } from "react-router-dom";
+import Wrapbar from "./Wrapbar";
 import Footer from "./Footer";
+
+import './styles/Layout.css';
 
 const Layout: React.FC = () => {
   return (
-    <div>
+    <div className="background-container">
+      <div className="mobile-frame">
         <Header />
-        <Outlet />
-        <Footer />
+        <div className="content-container">
+          <Outlet />
+          <Footer />
+        </div>
+        <Wrapbar />
+      </div>
     </div>
   );
 };
