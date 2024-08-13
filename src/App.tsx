b2914a8a56db5cc7b414a85a92945645c7a6693d
Harfe0626 <strik241@gmail.com>
2024-08-13 19:27:06 +0900

    Add : Setting page Structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Notice from "./routes/Notice";
 import ActivityRecommed from "./routes/ActivityRecommed";
 import OutfitByTemp from "./routes/OutfitByTemp";
 import SensitiveCheck from "./routes/SensitiveCheck";
-
+import Setting from "./routes/Setting";
 const App: React.FC = () => {
   return (
     <div>
@@ -19,6 +19,7 @@ const App: React.FC = () => {
             <Route path="/activityRecommed" element={<ActivityRecommed />} />
             <Route path="/outfitbytemp" element={<OutfitByTemp />} />
             <Route path="/sensitivecheck" element={<SensitiveCheck />} />
+            <Route path="/setting" element={<Setting />} />
           </Route>
         </Routes>
       </Router>
