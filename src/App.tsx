fcb5c40ce91d7374d0c916fde6c23fd1b0a525cc
Harfe0626 <strik241@gmail.com>
2024-08-13 15:57:49 +0900

    Add : OutfitByTemp page layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./routes/Home";
 import Notice from "./routes/Notice";
+import ActivityRecommed from "./routes/ActivityRecommed";
+import OutfitByTemp from "./routes/OutfitByTemp";
 
 const App: React.FC = () => {
   return (
@@ -13,6 +15,8 @@ const App: React.FC = () => {
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/notice" element={<Notice />} />
+            <Route path="/activityRecommed" element={<ActivityRecommed />} />
+            <Route path="/outfitbytemp" element={<OutfitByTemp />} />
           </Route>
         </Routes>
       </Router>
