72a489c75a371c2d3fb574441833e0d89fd0bc38
Recyclingbottle <hoholeo382@gmail.com>
2024-08-26 11:43:49 +0900

    sentry setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppWrapper from "./App";
 import "./index.css";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0c8cd129d0329f0efff4c991ed63be21@o4507837525327872.ingest.us.sentry.io/4507837670490112",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Set profilesSampleRate to 1.0 to profile every transaction.
+  // Since profilesSampleRate is relative to tracesSampleRate,
+  // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+  // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+  // results in 25% of transactions being profiled (0.5*0.5=0.25)
+  profilesSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
